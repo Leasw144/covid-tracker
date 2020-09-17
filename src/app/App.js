6822c1897b2d08d805647894d34a9447247dd67e
Sean Leas <phoenix_24leas.com>
2020-09-17 13:45:28 -0600

    Create a space for Nav and content #3

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,9 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 import logo from '../assets/logo.svg';
 import './App.css';
 
-import Header from '../header/Header'
+import Header from '../header/Header';
+import Navigation from '../navigation/Navigation';
+
 
 class App extends Component {
   constructor() {
@@ -19,18 +21,24 @@ class App extends Component {
 
       <main className="App">
         <Header />
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            FUCKING HERE WE FUCKING GO
-          </a>
+        
+        <section className="main-display">
+          <Navigation />
+          <div className="content">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              FUCKING HERE WE FUCKING GO
+            </a>
+          </div>
+        </section>
       </main>
     );
   }
