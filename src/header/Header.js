db812ac07c15fb352a740d49a93f73516af29d12
Sean Leas <phoenix_24leas.com>
2020-09-16 23:59:58 -0600

    Set up Header format #1

---
+++ src/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+import virus from '../assets/virus.svg';
+
+function Header() {
+  return (
+    <header>
+      <div className='horz-holder'>
+        <h1>CoVid-Tracker</h1>
+        <button  className='return-home-btn' type='button'> 
+          <img className='logo' src={virus} alt='virus'/>
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
