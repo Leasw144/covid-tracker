d599c95c9c9b12216ea429846cc252fa82002167
Sean Leas <phoenix_24leas.com>
2020-09-17 13:44:35 -0600

    Initialize css, test, and js #2

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navigation.css';
+
+function Navigation(props) {
+  return (
+    <article className='nav-bar'>
+      <h2>Resources</h2>
+
+    </article>
+  )
+}
+
+export default Navigation
