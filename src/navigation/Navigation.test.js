bba22291eb874e2d2425cbba8fc452a7e53cb2db
Sean Leas <phoenix_24leas.com>
2020-09-17 20:47:54 -0600

    Set up imports for Nav testing

---
+++ src/navigation/Navigation.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Navigation from './Navigation.js'
+
+describe('Navigation component', () => {
+  
+})
