cdc23e7bb702f47d9e4c54744f36b272e2ac004a
Sean Leas <phoenix_24leas.com>
2020-09-17 23:41:12 -0600

    Create and pass first two tests

--- src/navigation/Navigation.test.js
+++ src/navigation/Navigation.test.js
@@ -2,7 +2,28 @@ import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Navigation from './Navigation.js'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('Navigation component', () => {
-  
+  let heading, thirdPartyHeading, countries, submit;
+  beforeEach(() => {
+    countries = ['Afghanistan', 'Albania']
+    render (
+      <MemoryRouter>
+        <Navigation countries={countries}/>
+      </MemoryRouter>
+    ) 
+    submit = screen.getByRole('button', { name: 'submit'})
+    heading = screen.getByRole('heading', { name: 'Resources'})
+    thirdPartyHeading = screen.getByRole('heading', {name: 'Third Party Sources'})
+    
+  })
+
+  it('should render the Resources heading', () => {
+    expect(heading).toBeInTheDocument()
+    expect(thirdPartyHeading).toBeInTheDocument()
+  })
+  it('should render a submit button for countries', () => {
+    expect(submit).toBeInTheDocument()
+  })
 })
