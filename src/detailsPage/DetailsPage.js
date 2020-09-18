a6b147b68fb247d7da294710f2ae2fd96eb6f350
Sean Leas <phoenix_24leas.com>
2020-09-17 23:41:36 -0600

    Add dynamic stats to page

--- src/detailsPage/DetailsPage.js
+++ src/detailsPage/DetailsPage.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import './DetailsPage.css'
 
 function DetailsPage(props) {
+  console.log('your props', props)
+  let countryStats = props.country
   return (
-    <p>Hello World</p>
+    <article className="country-info">
+      <h1>{countryStats.Country} ({countryStats.CountryCode})</h1>
+    <p>New Confirmed Cases: {countryStats.NewConfirmed}</p>
+    <p>New Reported Deaths: {countryStats.NewDeaths}</p>
+    <p>New Recovered: {countryStats.NewRecovered}</p>
+    <br/>
+    <h2>Overall</h2>
+    <p>Total Confirmed: {countryStats.TotalConfirmed}</p>
+    <p>Total Deaths: {countryStats.TotalDeaths}</p>
+    <p>Total Recovered: {countryStats.TotalRecovered}</p>
+
+    </article>
   )
 }
 
