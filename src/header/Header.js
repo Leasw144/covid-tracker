44bf4c8a232512fb881ab3062a2efb586d7ecf47
Sean Leas <phoenix_24leas.com>
2020-09-18 18:22:07 -0600

    Add Link to Header

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Header.css';
 import virus from '../assets/virus.svg';
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <header>
       <div className='horz-holder'>
         <h1 className='title'>CoVid-Tracker</h1>
-        {/* <button  className='return-home-btn' type='button'>  */}
+        <Link to='/'>
           <img className='logo' src={virus} alt='virus'/>
-        {/* </button> */}
+        </Link>
       </div>
     </header>
   )
