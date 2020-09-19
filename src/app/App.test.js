0aebba8a1be9adbd0b9d6df2adce7bb29a3d2b81
Sean Leas <phoenix_24leas.com>
2020-09-18 18:47:48 -0600

    Set up testing for app

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App component', () => {
+  let countriesData, globalData;
+  beforeEach(() => {
+    countriesData = {
+      "Country": "United States of America",
+      "CountryCode": "US",
+      "Slug": "united-states",
+      "NewConfirmed": 32129,
+      "TotalConfirmed": 275582,
+      "NewDeaths": 1161,
+      "TotalDeaths": 7087,
+      "NewRecovered": 0,
+      "TotalRecovered": 0,
+      "Date": "2020-04-05T06:37:00Z"
+    },
+  })
+})
+
