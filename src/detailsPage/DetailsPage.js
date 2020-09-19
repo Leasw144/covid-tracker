f9e0a3cec69579ff4bd3511c87d7af5f90f8a61f
Sean Leas <phoenix_24leas.com>
2020-09-18 18:47:12 -0600

    Add cond rendering, place all global stats

--- src/detailsPage/DetailsPage.js
+++ src/detailsPage/DetailsPage.js
@@ -4,20 +4,31 @@ import './DetailsPage.css'
 function DetailsPage(props) {
   console.log('your props', props)
   let countryStats = props.country
+  if (countryStats.CountryCode) {
   return (
     <article className="country-info">
-      <h1>{countryStats.Country} ({countryStats.CountryCode})</h1>
-    <p>New Confirmed Cases: {countryStats.NewConfirmed}</p>
-    <p>New Reported Deaths: {countryStats.NewDeaths}</p>
-    <p>New Recovered: {countryStats.NewRecovered}</p>
+      <h1 className="country-name">{countryStats.Country} ({countryStats.CountryCode})</h1>
+      <h2>Recents</h2>
+      <div className="recent-group">
+        <p>New Confirmed Cases: 
+          <span class='red'>{countryStats && countryStats.NewConfirmed.toLocaleString()}</span>
+        </p>
+        <p>New Reported Deaths: <span class='red'>{countryStats.NewDeaths.toLocaleString()}</span></p>
+        <p>New Recovered: <span class='red'>{countryStats.NewRecovered.toLocaleString()}</span></p>
+      </div>
     <br/>
     <h2>Overall</h2>
-    <p>Total Confirmed: {countryStats.TotalConfirmed}</p>
-    <p>Total Deaths: {countryStats.TotalDeaths}</p>
-    <p>Total Recovered: {countryStats.TotalRecovered}</p>
-
+      <p>Total Confirmed: <span class='green'>{countryStats.TotalConfirmed.toLocaleString()}</span></p>
+      <p>Total Deaths: <span class='green'>{countryStats.TotalDeaths.toLocaleString()}</span></p>
+      <p>Total Recovered: <span class='green'>{countryStats.TotalRecovered.toLocaleString()}</span></p>
+      
     </article>
   )
+  } else {
+    return (
+      <h1>Soz</h1>
+    )
+  }
 }
 
 export default DetailsPage
