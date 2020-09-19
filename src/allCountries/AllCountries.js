73ccf923a01a7af74e4aef8151c904884f222b92
Sean Leas <phoenix_24leas.com>
2020-09-19 00:55:57 -0600

    Set up for conditional rendering of watch btn #4

--- src/allCountries/AllCountries.js
+++ src/allCountries/AllCountries.js
@@ -1,35 +1,51 @@
 import React, { Component } from 'react';
 import './AllCountries.css'
-import virus from '../assets/virus.svg';
+import eye from '../assets/eye.svg';
+import info from '../assets/info.svg';
 import { Link } from 'react-router-dom'
 
-function AllCountries (props) {
-  let counter = 0
-  let countries = props.allCountries
-  const countryCards = countries.map(country => {
-    return (
-      <div key={counter++} className="card">
-        <section className="recent-sect">
-        <h1 className="country-name">{country.Country}<span className="country-code">({country.CountryCode})</span></h1>
-          <h2>New Confirmed: <span className="integer">{country.NewConfirmed}</span></h2>
-          <h2>New Deaths: <span className="integer">{country.NewDeaths}</span></h2>
-          <h2>New Recovered: <span className="integer">{country.NewRecovered}</span></h2>
-        </section>
-        <section className="buttons">
-          <button type="button" className="watch-list">
-            <img src={virus} className="image" alt='virus' />
-          </button>
-          <Link to='/details'>
-            <button type='button' onClick={() => props.goToCountry(country.Country)}>
-              {/* <img src='' alt='details' /> */}
-              find more stats
-            </button>
-          </Link>
-        </section>
 
-      </div>
-    )
-  })
+function AllCountries (props) {
+  let countries, countryCards;
+  let counter = 0
+
+  if(props.countries) {
+    countries = props.countries
+    console.log('watchlist', props.watchList)
+    countryCards = countries.map(country => {
+      let watched;
+      
+      console.log('country', country.Country)
+      return (
+        <div key={counter++} className="card">
+          <section className="recent-sect">
+          <h1 className="country-name">{country.Country}<span className="country-code">({country.CountryCode})</span></h1>
+            <h2 className='stat'>New Confirmed: <span className="integer">{country.NewConfirmed}</span></h2>
+            <h2 className='stat'>New Deaths: <span className="integer">{country.NewDeaths}</span></h2>
+            <h2 className='stat'>New Recovered: <span className="integer">{country.NewRecovered}</span></h2>
+          </section>
+          <section className="button-holder">
+           
+              <button type="button" className="card-btn" onClick={() => props.addToWatch(country.Country)}>
+                <img src={eye} alt='virus' />
+              </button>
+            
+        
+         
+            <Link to='/details'>
+              <button className="card-btn" type='button' onClick={() => props.goToCountry(country.Country)}>
+                <img  src={info} alt='details' />
+                {/* find more stats */}
+              </button>
+            </Link>
+          </section>
+
+        </div>
+      )
+    })
+  } else {
+    countryCards = <h1> Error finding countries </h1>
+  }
   return (
     <div>
       {countryCards}
