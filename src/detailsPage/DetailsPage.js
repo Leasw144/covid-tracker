72b96916b500cd47a1b7affd85ea07349835f444
Sean Leas <phoenix_24leas.com>
2020-09-19 00:52:51 -0600

    Add watchList btn #4

--- src/detailsPage/DetailsPage.js
+++ src/detailsPage/DetailsPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './DetailsPage.css'
+import eye from '../assets/eye.svg'
 
 function DetailsPage(props) {
   console.log('your props', props)
@@ -8,7 +9,7 @@ function DetailsPage(props) {
   return (
     <article className="country-info">
       <h1 className="country-name">{countryStats.Country} ({countryStats.CountryCode})</h1>
-      <h2>Recents</h2>
+      <h2 class='headline'>Recents</h2>
       <div className="recent-group">
         <p>New Confirmed Cases: 
           <span class='red'>{countryStats && countryStats.NewConfirmed.toLocaleString()}</span>
@@ -17,11 +18,17 @@ function DetailsPage(props) {
         <p>New Recovered: <span class='red'>{countryStats.NewRecovered.toLocaleString()}</span></p>
       </div>
     <br/>
-    <h2>Overall</h2>
+      <h2 class='headline'>Overall</h2>
       <p>Total Confirmed: <span class='green'>{countryStats.TotalConfirmed.toLocaleString()}</span></p>
       <p>Total Deaths: <span class='green'>{countryStats.TotalDeaths.toLocaleString()}</span></p>
       <p>Total Recovered: <span class='green'>{countryStats.TotalRecovered.toLocaleString()}</span></p>
-      
+      <br />
+      <div className='eye-area'>
+        <h4>Add this Country to Your Watch List</h4>
+        <button type="button" className="card-btn" onClick={() => props.addToWatch(countryStats.Country)}>
+          <img className='eye' src={eye} alt='virus' />
+        </button>
+      </div>
     </article>
   )
   } else {
