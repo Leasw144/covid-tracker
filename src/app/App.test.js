2ae1a331da5a2dfe0ec2eadb1409e987d2346c0f
Sean Leas <phoenix_24leas.com>
2020-09-20 00:12:27 -0600

    Comment out setup for react for now #8

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter, Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+import '@testing-library/jest-dom/extend-expect'
 
-describe('App component', () => {
-  let countriesData, globalData;
-  beforeEach(() => {
-    countriesData = {
-      "Country": "United States of America",
-      "CountryCode": "US",
-      "Slug": "united-states",
-      "NewConfirmed": 32129,
-      "TotalConfirmed": 275582,
-      "NewDeaths": 1161,
-      "TotalDeaths": 7087,
-      "NewRecovered": 0,
-      "TotalRecovered": 0,
-      "Date": "2020-04-05T06:37:00Z"
-    },
-  })
-})
+// describe('App component', () => {
+//   let countriesData, globalData, history;
+//   beforeEach(() => {
+//     countriesData = {
+//       "Country": "United States of America",
+//       "CountryCode": "US",
+//       "Slug": "united-states",
+//       "NewConfirmed": 32129,
+//       "TotalConfirmed": 275582,
+//       "NewDeaths": 1161,
+//       "TotalDeaths": 7087,
+//       "NewRecovered": 0,
+//       "TotalRecovered": 0,
+//       "Date": "2020-04-05T06:37:00Z"
+//     }
+
+//   })
+//   it('tests everything I guess.', () => { 
+//     history = createMemoryHistory()
+//     const { container, getByText } = render(
+//       <Router history={history}>
+//         <App />
+//       </Router>
+//     )
+//     expect(container.innerHTML).toMatch('all countries')
+
+//   })
+// })
 
